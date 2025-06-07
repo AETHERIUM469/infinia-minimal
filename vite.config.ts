57cadec49c0eefdb0962afd4e4b82f8f86c4d562
AETHERIUM469 <sukesh.gpillayphysio@gmail.com>
2025-06-07 13:28:50 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000
-  }
-});
+  root: '.',              // Ensure this is set
+  publicDir: 'public',    // This tells Vite where to find index.html
+})
